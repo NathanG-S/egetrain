853f17dc5f3e49c6d78d96348a70aa0924965ef9
unknown <natan.gadzaev87@gmail.com>
2022-12-24 17:04:42 +0300

    24 number solution

--- number14.py
+++ number14.py
@@ -1,8 +1,8 @@
 # (№ 5495) (В. Шубинкин)
-for x in '0123456789abcdefghijkl':
+'''for x in '0123456789abcdefghijkl':
     for y in '0123456789abc':
         s = int(f'{x}23{x}5', 22) - int(f'67{y}9{y}', 13)
         if s % 57 == 0:
-            print(s / 57)
+            print(s / 57)'''
 
 # 25871
--- number24.py
+++ number24.py
@@ -12,6 +12,32 @@ new = [i[2:13] for i in num_lst]
 final_list = [i for i in new if i[0] == '4' and i[1] == '3' and i[4] == '7' and i[5] == '8' and i[9] == '3' and i[10] == '4']
 print(max(map(int, final_list)))
 
-#Output: 43997853334
-#3*9*9*7*5*3*3*3 = 229635 - answer on task
-#This script runs for about a minute because .txt file is too big.
+# Output: 43997853334
+# 3*9*9*7*5*3*3*3 = 229635 - answer on task
+# This script runs for about a minute because .txt file is too big.
+
+
+# #-буква $-цифра   #$Z#
+# буква цифра буква = #$#
+# #$#$##$##$##$##$##
+
+f = open('24_2.txt')
+s = f.readline()
+for i in s:
+    if i == 'A' or i == 'B' or i == 'C':
+        s = s.replace(i, '#')
+    if i == '1' or i == '2' or i == '3':
+        s = s.replace(i, '$')
+
+s = s.replace("$##", 'Z')
+
+counts = []
+cts = 0
+for i in range(len(s)):
+    if s[i] == 'Z':
+        cts += 1
+    if s[i] == 'Z' and s[i+1] != 'Z':
+        counts.append(cts)
+        cts = 0
+
+print(sorted(counts)[-1])
